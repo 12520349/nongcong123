ae0963451204ce3cd112ad0993bab4c8d6983e98
QN <50463070+12520349@users.noreply.github.com>
2022-10-12 16:59:34 +0700

    Update file_nap_code.ino

--- file_nap_code.ino
+++ file_nap_code.ino
@@ -1,10 +1,19 @@
+
+#define role 5
+#define ledPin 13
+int nguong = 500;
+#include <DHT.h> // Gọi thư viện DHT22
+
 #include "SIM900.h"
 #include <SoftwareSerial.h>
 #include "sms.h"
 #include "call.h"
 #define DEBUG_ON false
 #define DEBUG_PRINT false
-
+/****************8
+ #define _GSM_TXPIN_ 7
+#define _GSM_RXPIN_ 8
+** * * * * * * * */
 
 byte stat = 0;
 CallGSM call;
@@ -16,50 +25,92 @@ char n[20];
 unsigned long time;
 char sodienthoai[13] = "+84915269373";
 char noidung[160] = "Co Trom!";
-char phone_numberA[] = "0915269373";
+char phone_numberA[] = "0915269373"; // chinh so dien thoai o day
+int so_lan_rung = 0;
+unsigned long gioi_han_thoi_gian = 15000; //Khoang thoi gian giua 2 sms la: 15s
+int max_nhietdo = 35; // chinh gioi han nhiet do o day
 
-unsigned long gioi_han_thoi_gian = 20000; //Khoang thoi gian giua 2 sms la: 15s
- void setup() {
-   //Serial connection.
+int btn = 11;
+const int DHTPIN = A0; //Đọc dữ liệu từ DHT22 ở chân A3 trên mạch Arduino
+const int DHTTYPE = DHT22; //Khai báo loại cảm biến, có 2 loại là DHT11 và DHT22
+DHT dht(DHTPIN, DHTTYPE);
+ 
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A5, INPUT);
+  pinMode(role, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+    pinMode(btn, INPUT_PULLUP);
+ digitalWrite(role, LOW); // tat relay
+  Serial.begin(9600);
+  dht.begin(); // Khởi động cảm biến
    time = millis();
-   Serial.begin(9600);
-   Serial.println("GSM Shield testing.");
-   if (gsm.begin(2400)) {
-     Serial.println("\nstatus=READY");
-     started = true;
-   } else Serial.println("\nstatus=IDLE");
+   if (gsm.begin(2400)){     
+        Serial.println("\nstatus=READY");     
+        started=true;     
+    }   else 
+        Serial.println("\nstatus=IDLE");  
+    
+    if(started){   
+         Serial.print("gsm khoi dong xong! ");
+  
+      //  sms.SendSMS("+84387521116", "Online");   //đổi lại số của bạn nhé :D
+    } 
+}
 
-   if (started) {
-     //Enable this two lines if you want to send an SMS.
-     //if (sms.SendSMS("+84387521116", "Khoi dong OK!"))
-     //  Serial.println("\nSMS sent OK");
-   }
+void loop() {
+  int value_cambienmua = analogRead(A5);// analogRead(A5);
+    Serial.print("Gia tri cam bien mua: ");
+      Serial.println(value_cambienmua);
+  // put your main code here, to run repeatedly:
+   Serial.print("Gia tri cam bien rung: ");
 
- };
-
- void loop() {
-  stat = call.CallStatusWithAuth(sodienthoai, 1, 5); //kiểm tra trạng thái xem có cuộc gọi nào đến không
-  //Serial.println(stat);
-    //if(stat == CALL_INCOM_VOICE_AUTH) {
-     if(stat == 2) {
-       delay(500);
-       call.HangUp(); //gác máy khỏi tốn hehehe!
-       delay(200);
-        
+  int value_cambienrung = digitalRead(btn);// analogRead(A5);
+   Serial.println(value_cambienrung);
+   if (value_cambienrung == LOW ){
+       Serial.print("Phat hien RUNG");
+so_lan_rung++;
+   }else{
+    so_lan_rung = 0;
     }
-   int value = analogRead(A0); //A0 là chân analogA0 á
-   //nếu mà cảm biến rung đangrung :3 thì giá trị cảm biến nhảy ở khoảng 1022 - 1023 :) 
-   if (value < 1021) {
-     Serial.println("Khong Rung:");
-     //Serial.println(value); 
-   } else {
-     Serial.println("Rung");
-     if ((unsigned long)(millis() - time) > gioi_han_thoi_gian) {
-       //if (sms.SendSMS(sodienthoai, noidung)) {
-         call.Call(phone_numberA);
-         time = millis();
+   if (so_lan_rung >= 2){
+     // Serial.println( (unsigned long)(millis() - time));
+      if ((unsigned long)(millis() - time) > gioi_han_thoi_gian) {
+        so_lan_rung = 0;
          Serial.println("Thuc hien cuoc goi bao dong!");
-         delay(1000);
-         }
+       call.Call(phone_numberA);
+         time = millis();
+          delay(1000);
+        
+       
+        digitalWrite(ledPin, HIGH);
+        digitalWrite(role, HIGH); // CHAN COM <-> NC
+        delay(5000);
+        Serial.println("Da bat role");
+        }
+    }else{
+   delay(100);
+    //Serial.println("Da tat role");
+    digitalWrite(ledPin, LOW);
+    digitalWrite(role, LOW);
     }
- };
+
+    float h = dht.readHumidity(); //Đọc độ ẩm
+    float t = dht.readTemperature(); //Đọc nhiệt độ
+    Serial.print("Nhiet do hien tai: ");
+    Serial.println(t); //Xuất nhiệt độ
+    if (t >= max_nhietdo){
+      String stringOne = "Qua nhiet: "+ String(t) + " ^C";
+        char noidung_sms[100];
+        stringOne.toCharArray(noidung_sms, 100);
+      sms.SendSMS(phone_numberA, noidung_sms);
+       Serial.print("Da gui sms voi noi dung: ");
+            Serial.println(stringOne);
+      }
+      /*
+    Serial.print("Do am: ");
+    Serial.println(h); //Xuất độ ẩm
+ 
+    Serial.println(); //Xuống hàng
+    */
+}
